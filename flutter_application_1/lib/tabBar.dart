fd78472a9a68b37f06e3a8b9368c24b4d6e34f5e
Muhammad Nabeel Manzoor <nelsonted12@gmail.com>
2021-06-14 15:50:19 +0500

    work done

--- flutter_application_1/lib/tabBar.dart
+++ flutter_application_1/lib/tabBar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class TabBar extends StatefulWidget {
-  
-
   @override
   _TabBarState createState() => _TabBarState();
 }
@@ -10,8 +8,10 @@ class TabBar extends StatefulWidget {
 class _TabBarState extends State<TabBar> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 4, child: Scaffold(
-      appBar: AppBar(title:),
-    ));
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(title: Text("Hello")),
+        ));
   }
 }
